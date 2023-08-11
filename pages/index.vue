6d94b34686e76e20116f47c6230ee57112d1d7c1
Sakhile Simelane <51701132+sakhike1@users.noreply.github.com>
2023-08-11 06:11:45 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -21,8 +21,8 @@
                 <div
                   class="absolute transition-all duration-200 rounded-full -inset-px bg-gradient-to-r from-cyan-500 to-purple-500 group-hover:shadow-lg group-hover:shadow-cyan-500/50">
                 </div>
-                <button @click="$router.push('/admin')"
-                  class="relative inline-flex items-center justify-center px-8 py-3 text-base font-normal text-white bg-black border border-transparent rounded-full"
+                <button class="relative inline-flex items-center justify-center px-8 py-3 text-base font-normal text-white bg-black border border-transparent rounded-full"  @click="$router.push('/admin')"
+                  
                   role="button">
                   Signup to post your work
                 </button>
