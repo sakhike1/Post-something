5bfe615c8fd5ce57d4154ea716688f849cf4a90a
Sakhile Simelane <51701132+sakhike1@users.noreply.github.com>
2023-08-31 04:13:08 +0200

    Update AppButton.vue

--- components/UI/AppButton.vue
+++ components/UI/AppButton.vue
@@ -3,7 +3,13 @@
     :class="btnStyle"
     v-bind="$attrs"
     v-on="$listeners"
-    
+    style="
+      background: linear-gradient(
+        to right,
+        rgb(236, 72, 153),
+        rgb(239, 68, 68),
+        rgb(234, 179, 8)
+      );
     "
     class="inline-flex items-center px-6 py-3 text-gray-50 bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 rounded-full hover:bg-gray-200 hover:text-gray-300"
   >
