e4117b4045e29736a5b41bd7c1dbc140571a3a17
Sakhile Simelane <51701132+sakhike1@users.noreply.github.com>
2023-08-31 04:16:02 +0200

    Update AppButton.vue

--- components/UI/AppButton.vue
+++ components/UI/AppButton.vue
@@ -1,10 +1,6 @@
 <template>
-  <button
-    :class="btnStyle"
-    v-bind="$attrs"
-    v-on="$listeners"
-    class="inline-flex items-center px-6 py-3 text-gray-50 bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 rounded-full hover:bg-gray-200 hover:text-gray-300"
-  >
+  <button :class="btnStyle" v-bind="$attrs" v-on="$listeners"
+    class="inline-flex items-center px-6 py-3 text-gray-50 bg-black rounded-full hover:bg-gray-200 hover:text-gray-300">
     <slot />
   </button>
 </template>
